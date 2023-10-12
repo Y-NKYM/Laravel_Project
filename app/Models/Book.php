e0ae59887f4abc71f02f26bfa3b9aa0de307da07
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-10-12 07:08:16 +0000

    [Add] Migration BookTable UserPortfolioImageColumn, Book Model

---
+++ app/Models/Book.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+    
+    // テーブル名の指定
+    protected $table = 'books';
+    
+    // fillable or guarded
+    protected $fillable = 
+    [
+        'title',
+        'user_id',
+        'image',
+        'content'
+    ];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/book', [BookController::class, 'index'])->name('book.index');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
